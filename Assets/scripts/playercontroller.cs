3ed62782639c2df9ceaf0c44c7552e1171567409
Oreo1991 <183719394@qq.com>
2019-05-06 21:59:00 +0800

    animation update

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -22,24 +22,24 @@ public class playercontroller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector3.forward * vSpeed * Time.deltaTime);
-        float horizontalInput = Input.GetAxis("Horizontal") * hSpeed;
-        //float verticalInput = Input.GetAxis("Vertical") * speed;
-        Vector3 input = new Vector3(horizontalInput, rb.velocity.y, rb.velocity.z);
-        Vector3 rotatedInput = transform.TransformVector(input);
-        rb.velocity = rotatedInput;
-        //Debug.LogError(rotatedInput.ToString());
+        //transform.Translate(Vector3.forward * vSpeed * Time.deltaTime);
+        //float horizontalInput = Input.GetAxis("Horizontal") * hSpeed;
+        ////float verticalInput = Input.GetAxis("Vertical") * speed;
+        //Vector3 input = new Vector3(horizontalInput, rb.velocity.y, rb.velocity.z);
+        //Vector3 rotatedInput = transform.TransformVector(input);
+        //rb.velocity = rotatedInput;
+        ////Debug.LogError(rotatedInput.ToString());
 
-        transform.Rotate(0, 0, 0);
-        Camera.main.transform.parent.Rotate(0, 0, 0);
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.velocity = new Vector3(rb.velocity.x, height, rb.velocity.z);
-            //Debug.LogError(rb.velocity.ToString());
-            //Debug.LogError(this.transform.position);
-            //anim.SetTrigger("Jump");
-        }
-        //anim.SetFloat("VerticalSpeed", verticalInput);
-        //anim.SetFloat("HorizontalSpeed", horizontalInput);
+        //transform.Rotate(0, 0, 0);
+        //Camera.main.transform.parent.Rotate(0, 0, 0);
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    rb.velocity = new Vector3(rb.velocity.x, height, rb.velocity.z);
+        //    //Debug.LogError(rb.velocity.ToString());
+        //    //Debug.LogError(this.transform.position);
+        //    //anim.SetTrigger("Jump");
+        //}
+        ////anim.SetFloat("VerticalSpeed", verticalInput);
+        ////anim.SetFloat("HorizontalSpeed", horizontalInput);
     }
 }
